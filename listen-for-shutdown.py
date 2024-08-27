b8507874aeae2bd68595ae43038aaded6a81569c
N6RDV <97379462+N6RDV@users.noreply.github.com>
2024-08-27 14:39:44 -0700

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -6,23 +6,33 @@ import subprocess
 import time
 from time import sleep
 
-gpio_pin=3
-long_press=0
+button_gpio=3
+relay_gpio=4
+
+short_press=0.5
+long_press=10
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(relay_gpio, GPIO.OUT)
 
 def main():
+  relay_state=False
+  GPIO.output(relay_gpio, relay_state)
   while True:
-    GPIO.wait_for_edge(gpio_pin, GPIO.FALLING)
+    GPIO.wait_for_edge(button_gpio, GPIO.FALLING)
     counter = 0
-    while GPIO.input(gpio_pin) == 0 and counter < long_press:
+    while GPIO.input(button_gpio) == 0 and counter < long_press:
       time.sleep(0.1)
-      counter += 1
+      counter = round(counter + 0.1, 1)
       print('Shutdown button is pressed ' + str(counter))
     if counter >= long_press:
       print('Shutting down')
       subprocess.call(['shutdown', '-h', 'now'], shell=False)
+    elif counter >= short_press:
+      relay_state = not relay_state
+      print('Changing relay state to: ' + str(relay_state))
+      GPIO.output(relay_gpio, relay_state)
 
 if __name__ == '__main__':
   main()
